a0210678ce7a868cde5a2f970ea557869ac8f85e
SaulDV <felix21soc@gmail.com>
2026-01-19 16:40:13 -0500

    añadido seo y paginas de terminos y servicios

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,16 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   },
-
-  integrations: [react()]
+  site: "https://version-counter.netlify.app",
+  integrations: [react(), sitemap()],
 });
